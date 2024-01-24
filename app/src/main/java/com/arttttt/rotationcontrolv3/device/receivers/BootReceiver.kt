9fca7f28ec52a39c40a270f9abed5d1ff8d04d4f
arttttt <artem-bambalov@yandex.ru>
2024-01-25 00:09:40 +0200

    add empty boot receiver

---
+++ app/src/main/java/com/arttttt/rotationcontrolv3/device/receivers/BootReceiver.kt
@@ -0,0 +1,11 @@
+package com.arttttt.rotationcontrolv3.device.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+    }
+}

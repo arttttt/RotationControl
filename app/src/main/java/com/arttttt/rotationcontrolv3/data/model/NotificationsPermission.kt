76f6644ce81c72876fb56f25dbea0b71ab14e0ef
arttttt <artem-bambalov@yandex.ru>
2024-01-26 02:09:21 +0200

    add check status default implementation

--- app/src/main/java/com/arttttt/rotationcontrolv3/data/model/NotificationsPermission.kt
+++ app/src/main/java/com/arttttt/rotationcontrolv3/data/model/NotificationsPermission.kt
@@ -1,12 +1,9 @@
 package com.arttttt.rotationcontrolv3.data.model
 
 import android.Manifest
-import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
-import com.arttttt.permissions.domain.entity.Permission
 import com.arttttt.permissions.domain.entity.StandardPermission
-import com.arttttt.permissions.utils.extensions.checkStatusImpl
 
 data object NotificationsPermission : StandardPermission by if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) StandardNoOpPermission else Impl33 {
 
@@ -14,9 +11,5 @@ data object NotificationsPermission : StandardPermission by if (Build.VERSION.SD
     private data object Impl33 : StandardPermission {
 
         override val permission: String = Manifest.permission.POST_NOTIFICATIONS
-
-        override fun checkStatus(context: Context): Permission.Status {
-            return checkStatusImpl(context)
-        }
     }
 }
--- core/permissions/src/main/java/com/arttttt/permissions/domain/entity/StandardPermission.kt
+++ core/permissions/src/main/java/com/arttttt/permissions/domain/entity/StandardPermission.kt
@@ -1,6 +1,13 @@
 package com.arttttt.permissions.domain.entity
 
+import android.content.Context
+import com.arttttt.permissions.utils.extensions.checkStatusImpl
+
 interface StandardPermission : Permission {
 
     val permission: String
+
+    override fun checkStatus(context: Context): Permission.Status {
+        return checkStatusImpl(context)
+    }
 }

904afe5fdac831028b6c6d3d3efc84dca1cd69de
arttttt <artem-bambalov@yandex.ru>
2024-01-30 23:36:54 +0200

    add parcelize plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.google.ksp) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
 }

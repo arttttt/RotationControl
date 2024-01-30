beac05dfdc35c1e815b8ac5f857d83f2f24bf1f6
arttttt <artem-bambalov@yandex.ru>
2024-01-31 01:39:13 +0200

    add permissions repository impl

--- app/src/main/java/com/arttttt/rotationcontrolv3/domain/repository/PermissionsRepository.kt
+++ app/src/main/java/com/arttttt/rotationcontrolv3/domain/repository/PermissionsRepository.kt
@@ -1,10 +1,8 @@
-package com.arttttt.permissions.domain.repository
+package com.arttttt.rotationcontrolv3.domain.repository
 
 import com.arttttt.permissions.domain.entity.Permission
 
 interface PermissionsRepository {
-
-    fun getRequiredPermissions(): List<Permission>
 
     fun checkPermission(permission: Permission): Permission.Status
 }

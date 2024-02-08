654faa75c53c68f899eb65bb015097711de5dc79
arttttt <artem-bambalov@yandex.ru>
2024-02-08 23:47:55 +0200

    add ability to start on boot

--- app/src/main/java/com/arttttt/rotationcontrolv3/data/framework/BootReceiver.kt
+++ app/src/main/java/com/arttttt/rotationcontrolv3/data/framework/BootReceiver.kt
@@ -3,9 +3,20 @@ package com.arttttt.rotationcontrolv3.data.framework
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.core.content.ContextCompat
+import com.arttttt.rotationcontrolv3.ui.rotation.RotationService
 
 class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action != Intent.ACTION_BOOT_COMPLETED) return
+
+        ContextCompat.startForegroundService(
+            context,
+            Intent(
+                context,
+                RotationService::class.java,
+            ),
+        )
     }
 }

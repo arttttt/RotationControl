a7011d051ec110ab60abb06607542a1b4748f700
arttttt <artem-bambalov@yandex.ru>
2024-02-19 23:56:21 +0200

    bump up app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.arttttt.rotationcontrolv3"
         minSdk = 16
         targetSdk = 34
-        versionCode = 10
-        versionName = "2.0.0"
+        versionCode = 11
+        versionName = "2.0.1"
 
         multiDexEnabled = true
     }

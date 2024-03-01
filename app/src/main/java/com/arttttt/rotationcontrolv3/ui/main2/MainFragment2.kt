e96e71cc4d741388e2bb877f10eb3449e3ff91df
arttttt <artem-bambalov@yandex.ru>
2024-03-01 10:50:09 +0200

    add new main fragment

---
+++ app/src/main/java/com/arttttt/rotationcontrolv3/ui/main2/MainFragment2.kt
@@ -0,0 +1,6 @@
+package com.arttttt.rotationcontrolv3.ui.main2
+
+import androidx.fragment.app.Fragment
+import com.arttttt.rotationcontrolv3.R
+
+class MainFragment2 : Fragment(R.layout.fragment_main2)

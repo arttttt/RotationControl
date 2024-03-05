62e99d88355915effe68ecd95b92141a72a6447f
arttttt <artem-bambalov@yandex.ru>
2024-03-06 00:02:00 +0200

    add empty apps repository

---
+++ app/src/main/java/com/arttttt/rotationcontrolv3/data/repository/AppsRepositoryImpl.kt
@@ -0,0 +1,8 @@
+package com.arttttt.rotationcontrolv3.data.repository
+
+import android.content.Context
+import com.arttttt.rotationcontrolv3.domain.repository.AppsRepository
+
+class AppsRepositoryImpl(
+    private val context: Context,
+) : AppsRepository

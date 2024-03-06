f799eeae837291aaf991ff306c3ae658226b614f
arttttt <artem-bambalov@yandex.ru>
2024-03-06 23:15:36 +0200

    add toolbars

--- app/src/main/java/com/arttttt/rotationcontrolv3/ui/main2/MainFragment2.kt
+++ app/src/main/java/com/arttttt/rotationcontrolv3/ui/main2/MainFragment2.kt
@@ -55,21 +55,21 @@ class MainFragment2(
         data object Settings : MenuItem {
 
             override val id: Int = R.id.item_settings
-            override val title: Int = R.string.menu_item_settings
+            override val title: Int = R.string.settings
             override val icon: Int = R.drawable.ic_settings_24
         }
 
         data object About : MenuItem {
 
             override val id: Int = R.id.item_about
-            override val title: Int = R.string.menu_item_about
+            override val title: Int = R.string.about
             override val icon: Int = R.drawable.ic_info_24
         }
 
         data object Apps : MenuItem {
 
             override val id: Int = R.id.item_apps
-            override val title: Int = R.string.menu_item_apps
+            override val title: Int = R.string.apps
             override val icon: Int = R.drawable.ic_apps_24
         }
     }

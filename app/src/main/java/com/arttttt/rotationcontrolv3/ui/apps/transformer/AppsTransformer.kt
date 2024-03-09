9f00ccde21f4a48f46a626cf4e3d8aaf889d823a
arttttt <artem-bambalov@yandex.ru>
2024-03-09 18:49:52 +0200

    check if accessibility service is enabled

--- app/src/main/java/com/arttttt/rotationcontrolv3/ui/apps/transformer/AppsTransformer.kt
+++ app/src/main/java/com/arttttt/rotationcontrolv3/ui/apps/transformer/AppsTransformer.kt
@@ -1,9 +1,13 @@
 package com.arttttt.rotationcontrolv3.ui.apps.transformer
 
 import android.content.Context
+import com.arttttt.rotationcontrolv3.RotationControlAccessibilityService
 import com.arttttt.rotationcontrolv3.domain.stores.apps.AppsStore
+import com.arttttt.rotationcontrolv3.ui.apps.adapter.models.AccessibilityListItem
 import com.arttttt.rotationcontrolv3.ui.apps.adapter.models.AppAdapterItem
 import com.arttttt.rotationcontrolv3.ui.apps.view.AppsView
+import com.arttttt.rotationcontrolv3.utils.adapterdelegates.ListItem
+import com.arttttt.rotationcontrolv3.utils.extensions.isAccessibilityServiceEnabled
 import com.arttttt.rotationcontrolv3.utils.mvi.Transformer
 import javax.inject.Inject
 
@@ -13,13 +17,23 @@ class AppsTransformer @Inject constructor(
 
     override fun invoke(state: AppsStore.State): AppsView.Model {
         return AppsView.Model(
-            items = state.apps.map { info ->
+            items = createItems(state),
+        )
+    }
+
+    private fun createItems(state: AppsStore.State): List<ListItem> {
+        return if (context.isAccessibilityServiceEnabled<RotationControlAccessibilityService>()) {
+            state.apps.map { info ->
                 AppAdapterItem(
                     title = info.title,
                     appPackage = info.pkg,
                     icon = context.packageManager.getApplicationIcon(info.pkg),
                 )
-            },
-        )
+            }
+        } else {
+            listOf(
+                AccessibilityListItem
+            )
+        }
     }
 }
